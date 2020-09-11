dd77d2f0da108c1ee1a53fa0b6b294fcf3249efc
Jimmy <nunu3041@gmail.com>
2020-09-11 16:27:22 +0900

    Make RTMP_CHUNK_SIZE configurable

--- nginx-rtmp/run_nginx.sh
+++ nginx-rtmp/run_nginx.sh
@@ -4,6 +4,7 @@ set -e
 
 export NGINX_STAT_PORT=${NGINX_STAT_PORT:-8080}
 export RTMP_ALLOW_PUBLISH=${RTMP_ALLOW_PUBLISH:-127.0.0.1}
+export RTMP_CHUNK_SIZE=${RTMP_CHUNK_SIZE:-4096}
 
 envsubst < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
 
