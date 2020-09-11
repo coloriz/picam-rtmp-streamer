af31a91de463aa6e981701f5b755c527a3f3a3f3
Jimmy <nunu3041@gmail.com>
2020-09-11 17:01:19 +0900

    Make RTMP_BUFLEN configurable

--- nginx-rtmp/run_nginx.sh
+++ nginx-rtmp/run_nginx.sh
@@ -5,6 +5,7 @@ set -e
 export NGINX_STAT_PORT=${NGINX_STAT_PORT:-8080}
 export RTMP_ALLOW_PUBLISH=${RTMP_ALLOW_PUBLISH:-127.0.0.1}
 export RTMP_CHUNK_SIZE=${RTMP_CHUNK_SIZE:-4096}
+export RTMP_BUFLEN=${RTMP_BUFLEN:-1000ms}
 
 envsubst < /etc/nginx/templates/nginx.conf.template > /etc/nginx/nginx.conf
 
